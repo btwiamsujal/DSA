9f7744bf09735e13cbc1e6e766c5678611b054f6
Sujal Patel <125170841+btwiamsujal@users.noreply.github.com>
2025-06-02 10:48:15 +0530

    Time: 812 ms (45.33%) | Memory: 13.1 MB (61.19%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Two Sum
+        q = []
+        for i in range(len(nums)):
+            ans = target - nums[i]
+            if ans in nums and nums.index(ans) != i:
+                idx = nums.index(ans)
+                q.append(i)
+                q.append(idx)
+                break
+        return q
