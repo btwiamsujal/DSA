48c0f6c91a78945891ed3f0d82df2dcf4a9b3b8a
Sujal Patel <125170841+btwiamsujal@users.noreply.github.com>
2025-06-03 09:22:04 +0530

    Time: 8 ms (63.55%) | Memory: 13.9 MB (60.98%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        result = []
+        for candidate in [candidate1, candidate2]:
+            if candidate is not None and nums.count(candidate) > len(nums) // 3:
+                result.append(candidate)
+
+        return result
