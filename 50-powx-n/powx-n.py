e55a486e0655a7c78c8c69dafe69e1ae0204e626
Sujal Patel <125170841+btwiamsujal@users.noreply.github.com>
2025-06-03 10:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (26.00%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        ans = x ** n
+        return ans
+        # return f"{ans:.5f}"
