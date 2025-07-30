f060919a7d81de4f3a35eb9e4cac839d2141c99f
Sujal Patel <125170841+btwiamsujal@users.noreply.github.com>
2025-07-30 09:26:28 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (8.63%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         if not nums:
             return 0
-        idx = 0
+        unique_index = 0
         for i in range(1, len(nums)):
-            if nums[i] != nums[idx]:
-                idx += 1
-                nums[idx] = nums[i]
-        return idx + 1
+            if nums[i] != nums[unique_index]:
+                unique_index += 1
+                nums[unique_index] = nums[i]
+        return unique_index + 1
