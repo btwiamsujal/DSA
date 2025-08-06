75eb7a9651c7f0f7c8c431c34ee0b09c38810b8c
Sujal Patel <125170841+btwiamsujal@users.noreply.github.com>
2025-08-06 15:41:53 +0530

    Time: 1556 ms (100.00%) | Memory: 27.8 MB (55.56%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.py
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.py
@@ -1,4 +1,3 @@
-import bisect
 class Solution(object):
     def numOfUnplacedFruits(self, fruits, baskets):
         """
@@ -6,47 +5,32 @@ class Solution(object):
         :type baskets: List[int]
         :rtype: int
         """
-       
         n = len(baskets)
-        segment_tree = [0]*(4*n)
-
-        def build(ind, l, r):
-            if l==r:
-                segment_tree[ind] = baskets[l]
-                return
-            m = (l+r)//2
-            build(2*ind+1, l, m)
-            build(2*ind+2, m+1, r)
-            segment_tree[ind] = max(segment_tree[2*ind+1], segment_tree[2*ind+2])
-
-        def query(ind, l, r, val):
-            if segment_tree[ind] < val:
-                return (float('-inf'), -1)
-            if l==r:
-                return (segment_tree[ind], ind)
-            m = (l+r)//2
-            left = query(2*ind+1, l, m,val)
-            if left[0]>=val:
-                return left
-            right = query(2*ind+2, m+1, r, val)
-            if right[0] >= val:
-                return right
-            return (float('-inf'), -1)
-
-        def update(ind):
-            if ind<0:
-                return
-            lc = segment_tree[2*ind + 1]
-            rc = segment_tree[2*ind + 2]
-            segment_tree[ind] = max(lc, rc)
-            update((ind - 1)//2)
-        build(0, 0, n-1)
-        unplaced = 0
-        for f in fruits:
-            ind = query(0, 0, n-1, f)[1]
-            if ind != -1:
-                segment_tree[ind] = -1
-                update((ind - 1)//2)
+        s = 1
+        while s < n:
+            s *= 2
+        t = [0] * (2 * s)
+        
+        for i in range(n):
+            t[s + i] = baskets[i]
+        for i in range(s - 1, 0, -1):
+            t[i] = max(t[2 * i], t[2 * i + 1])
+        
+        c = 0
+        for x in fruits:
+            if t[1] < x:
+                c += 1
             else:
-                unplaced += 1
-        return unplaced
+                i = 1
+                while i < s:
+                    if t[2*i] >= x:
+                        i = 2*i
+                    else:
+                        i = 2*i+1
+                j = i - s
+                t[i] = -1
+                i //= 2
+                while i:
+                    t[i] = max(t[2*i], t[2*i+1])
+                    i //= 2
+        return c
